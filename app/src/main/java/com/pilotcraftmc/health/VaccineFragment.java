5038fdc8816fbbc4dbfdd7c2184966cbff8b1a17
DamianPilot382 <damianugalde@gmail.com>
2016-02-05 08:27:25 -0800

    Add Vaccine Youtube section

--- app/src/main/java/com/pilotcraftmc/health/VaccineFragment.java
+++ app/src/main/java/com/pilotcraftmc/health/VaccineFragment.java
@@ -1,10 +1,13 @@
 package com.pilotcraftmc.health;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -26,6 +29,36 @@ public class VaccineFragment extends Fragment {
 
         // Set the adapter for the list view
         mDrawerList.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.vaccine_list_item, mItems));
+
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Intent i = null;
+
+                switch(position){
+                    case 0:
+                        i = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:Rzxr9FeZf1g"));
+                        break;
+                    case 1:
+                        i = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:s_6QW9sNPEY"));
+                        break;
+                    case 2:
+                        i = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:cgymkPp6flg"));
+                        break;
+                    case 3:
+                        i = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:mLHnUjhzv9E"));
+                        break;
+                    default:
+                        break;
+                };
+                startActivity(i);
+
+            }
+        });
+
+
         return rootView;
     }
 
